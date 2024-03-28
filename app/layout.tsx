64e8f321b072d1bd08fb1c633ef9ddbafeb359c3
BabyLy233 <2105451348@qq.com>
2024-03-28 23:37:30 +0800

    Chapter 6

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,11 +1,14 @@
+import '@/app/ui/global.css';
+import { mono } from './ui/fonts';
+
 export default function RootLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
   return (
-    <html lang="en">
-      <body>{children}</body>
+    <html lang="zh-CN">
+      <body className={`${mono.className} antialiased`}>{children}</body>
     </html>
   );
 }
---
+++ app/ui/fonts.ts
@@ -0,0 +1,8 @@
+import { JetBrains_Mono, Lusitana } from 'next/font/google';
+
+export const mono = JetBrains_Mono({ subsets: ['latin'] });
+
+export const lusitana = Lusitana({
+  weight: ['400', '700'],
+  subsets: ['latin'],
+});
